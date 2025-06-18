7e74d1fe123691bb57715918fac625483b66aa33
Matteo <matteojandrade@gmail.com>
2025-06-18 19:12:11 -0400

    Finding links to each team's website

--- scrape.py
+++ scrape.py
@@ -7,12 +7,18 @@ url = "https://fbref.com/en/comps/9/Premier-League-Stats"
 html = requests.get(url)
 
 # Get the table from the HTML
-soup = BeautifulSoup(html.text)
+soup = BeautifulSoup(html.text, "lxml")
 table = soup.select("table.stats_table")[0]
 links = table.find_all("a")
 
-# Go to each team's website
+# Get the link for each team's website
+link_lst = []
 for l in links:
-    l.get("href")
-
-print(l)
+    link_lst.append(l.get("href"))
+teams = []
+for l in link_lst:
+    if "/squads/" in l:
+        teams.append(l)
+team_links = [f"https://fbref.com{t}" for t in teams]
+    
+print("\n", team_links)
