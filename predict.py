ce921fb952f11643eae8faf35ac5145c1dbde16f
Matteo <matteojandrade@gmail.com>
2025-06-29 12:12:27 -0400

    Clean the data

--- predict.py
+++ predict.py
@@ -1,7 +1,12 @@
 import pandas as pd
 # ^^^ imports ^^^
 
+# Read in the data
 matches = pd.read_csv("matches.csv", index_col=0)
 
+# Clean the data
+matches["date"] = pd.to_datetime(matches["Date"])
+matches.drop("Date", axis=1, inplace=True)
+
 print(matches.head())
-print(matches.shape)
+print(matches.dtypes)
