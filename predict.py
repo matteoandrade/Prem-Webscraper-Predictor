7045b8d7f17fdda14ff6830241be226ed037ac3d
Matteo <matteojandrade@gmail.com>
2025-06-30 10:32:15 -0400

    Preparing data

--- predict.py
+++ predict.py
@@ -8,5 +8,12 @@ matches = pd.read_csv("matches.csv", index_col=0)
 matches["date"] = pd.to_datetime(matches["Date"])
 matches.drop("Date", axis=1, inplace=True)
 
+# Make numerical values out of strings
+matches["venue_num"] = matches["Venue"].astype("category").cat.codes
+matches["opp_num"] = matches["Opponent"].astype("category").cat.codes
+matches["hour"] = matches["Time"].str.replace(":.+", "", regex=True).astype("int")
+matches["day_num"] = matches["date"].dt.day_of_week
+matches["obj"] = (matches["Result"] == 'W').astype("int")
+
 print(matches.head())
 print(matches.dtypes)
