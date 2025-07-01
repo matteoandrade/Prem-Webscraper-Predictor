3b3414ceb6f8bafd3382b7bf3480dda591a89c06
Matteo <matteojandrade@gmail.com>
2025-07-01 18:54:20 -0400

    The machine is starting to learn

--- predict.py
+++ predict.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
 # ^^^ imports ^^^
 
 # Read in the data
@@ -15,5 +18,20 @@ matches["hour"] = matches["Time"].str.replace(":.+", "", regex=True).astype("int
 matches["day_num"] = matches["date"].dt.day_of_week
 matches["obj"] = (matches["Result"] == 'W').astype("int")
 
-print(matches.head())
-print(matches.dtypes)
+# Breaking up data
+rf = RandomForestClassifier(n_estimators=50, min_samples_split=10, random_state=1)
+train = matches[matches["date"] < "2025-01-01"]
+test  = matches[matches["date"] >= "2025-01-01"]
+predictors = ["venue_num", "opp_num", "hour", "day_num"]
+
+# Fitting data
+rf.fit(train[predictors], train["obj"])
+preds = rf.predict(test[predictors])
+
+# Testing accuracy
+acc = accuracy_score(test["obj"], preds)
+combined = pd.DataFrame(dict(actual=test["obj"], prediction=preds))
+pd.crosstab(index=combined["actual"], columns=combined["prediction"])
+prec = precision_score(test["obj"], preds)
+
+print("Accuracy score:", acc, "Prediction score:", prec)
